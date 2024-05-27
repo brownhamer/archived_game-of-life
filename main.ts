14d3cbd289fc4c00bac85a2f7edf39356b864b7f
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-27 20:25:05 +0200

    First contact, rather quickly though

--- main.ts
+++ main.ts
@@ -1,22 +1,27 @@
-radio.onReceivedString(function (receivedString) {
-    receivedLanguage = greetings.indexOf(receivedString)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == language) {
+        repeatLanguage = 2
+    }
 })
-let receivedLanguage = 0
-let greetings: string[] = []
-greetings = ["Hello", "Hallo", "Hola"]
-let language = randint(0, 2)
-receivedLanguage = -1
+let repeatLanguage = 0
+let language = 0
+language = randint(0, 2)
+repeatLanguage = 0
 radio.setGroup(1)
-loops.everyInterval(500, function () {
-    if (receivedLanguage == language) {
+radio.setTransmitPower(7)
+radio.setFrequencyBand(0)
+loops.everyInterval(1000, function () {
+    control.waitMicros(randint(1, 123))
+    if (repeatLanguage > 0) {
         pins.analogWritePin(AnalogPin.P13, 0)
         pins.analogWritePin(AnalogPin.P14, 1023)
+        repeatLanguage += -1
     } else {
         pins.analogWritePin(AnalogPin.P13, 1023)
         pins.analogWritePin(AnalogPin.P14, 0)
         language = randint(0, 2)
+        repeatLanguage = 0
     }
     basic.showNumber(language)
-    radio.sendString("" + (greetings[language]))
-    receivedLanguage = -1
+    radio.sendNumber(language)
 })
