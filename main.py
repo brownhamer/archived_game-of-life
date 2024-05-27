8912a2feaa2f2d145944b8c31ac2397f9e9e0c2b
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-27 20:09:42 +0200

    Using Blocks to make sure stuff works first

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
+def on_received_string(receivedString):
+    global language
+    if receivedString == "Hello":
+        language = 0
+    else:
+        if receivedString == "Hallo":
+            language = 1
+        else:
+            language = 2
+radio.on_received_string(on_received_string)
+
+language = 0
+repeatLanguage = False
 greetings = ["Hello", "Hallo", "Hola"]
 language = randint(0, 2)
-repeatLanguage = False
-
-def on_received_string(receivedString):
-    global greetings
-    global language
-    receivedLanguage = greetings.index(receivedString)
-    if (receivedLanguage = language)
-        global repeatLanguage
-        repeatLanguage = True
-    pass
-
-def on_500_interval():
-    global greetings
-    global language
-    global repeatLanguage
-    if (repeatLanguage)
-        pins.analog_write_pin(AnalogPin.P13, 0)
-        pins.analog_write_pin(AnalogPin.P14, 666)
-        repeatLanguague = False
-    else
-        pins.analog_write_pin(AnalogPin.P13, 666)
-        pins.analog_write_pin(AnalogPin.P14, 0)
-        language = randint(0, 2)
-    basic.show_number(language)
-    radio.send_string(greetings[language])
-    pass
-
-def on_forever():
-    pass
-
 radio.set_group(1)
-radio.on_received_string(on_received_string)
-loops.every_interval(500, on_500_interval)
-basic.forever(on_forever)
+
+def on_every_interval():
+    pass
+loops.every_interval(500, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
+radio.onReceivedString(function (receivedString) {
+    receivedLanguage = greetings.indexOf(receivedString)
+})
 let receivedLanguage = 0
-let repeatLanguage = false
-let greetings = ["Hello", "Hallo", "Hola"]
+let greetings: string[] = []
+greetings = ["Hello", "Hallo", "Hola"]
 let language = randint(0, 2)
+receivedLanguage = -1
 radio.setGroup(1)
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    
-    receivedLanguage = _py.py_array_index(greetings, receivedString)
-})
-loops.everyInterval(500, function on_500_interval() {
-    
-})
-basic.forever(function on_forever() {
-    
+loops.everyInterval(500, function () {
+    if (receivedLanguage == language) {
+        pins.analogWritePin(AnalogPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P14, 1023)
+    } else {
+        pins.analogWritePin(AnalogPin.P13, 1023)
+        pins.analogWritePin(AnalogPin.P14, 0)
+        language = randint(0, 2)
+    }
+    basic.showNumber(language)
+    radio.sendString("" + (greetings[language]))
+    receivedLanguage = -1
 })
