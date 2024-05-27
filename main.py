4aaa6310058b783fc4f17d315f68dedd5117f8d8
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-27 19:20:25 +0200

    Starting with something

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+greetings = ['Hello', 'Hallo', 'Hola']
+
+def on_received_string(receivedString):
+    pass
+radio.on_received_string(on_received_string)
+
+radio.set_group(1)
+
 def on_forever():
     pass
-
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let greetings = ["Hello", "Hallo", "Hola"]
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
+})
+radio.setGroup(1)
 basic.forever(function on_forever() {
     
 })
