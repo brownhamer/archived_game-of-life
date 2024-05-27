b08c5510ffd5e7625bf11f5b9ccb31fcc32c4b09
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-27 19:33:25 +0200

    First contact maybe

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+language = randint(0, 2)
+languageRepeat = False
 greetings = ['Hello', 'Hallo', 'Hola']
 
 def on_received_string(receivedString):
+    neighborLanguage = greetings.index(receivedString)
+    if (neighborLanguage == language)
+        languageRepeat = True
+        basic.show_icon(IconNames.HEART)
+    else
+        basic.show_icon(IconNames.NO)
     pass
 radio.on_received_string(on_received_string)
+
+def onEvery_interval():
+    if (not languageRepeat)
+        language = randint(0, 2)
+
+    radio.send_string(greetings[language])
+    pass
+loops.every_interval(500, onEvery_interval)
 
 radio.set_group(1)
 
--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+let language = randint(0, 2)
 let greetings = ["Hello", "Hallo", "Hola"]
 radio.onReceivedString(function on_received_string(receivedString: string) {
+    let neighborLanguage = _py.py_array_index(greetings, receivedString)
     
 })
 radio.setGroup(1)
